65f4dd05fec6376c68e913ddc4cd2b08bbfea475
Felipe Silvério <felipe.silverio@triasoftware.com.br>
2021-06-02 18:50:13 -0300

    Fim do Segundo dia - Rotas Criadas e Componente da segunda tela criado, quase terminando a requisiçãod e API

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './components/search/search.component';
+import { WeatherViewComponent } from './components/weather-view/weather-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: SearchComponent },
+  { path: "weather", component: WeatherViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SearchComponent } from './components/search/search.component';
+import { WeatherViewComponent } from './components/weather-view/weather-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    WeatherViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
