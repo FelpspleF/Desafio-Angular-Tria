4e8682d094c3aad594551738c3cba269ae9e136b
Felipe Silvério <felipe.silverio@triasoftware.com.br>
2021-06-04 15:19:52 -0300

    rotas finalizadas, começo da requisição de api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SearchComponent } from './components/search/search.component';
 import { WeatherViewComponent } from './components/weather-view/weather-view.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { WeatherViewComponent } from './components/weather-view/weather-view.com
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
