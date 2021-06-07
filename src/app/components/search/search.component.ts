4802f3ccd57e21e62f7af1a180e761b57992fd57
Felipe Silvério <felipe.silverio@triasoftware.com.br>
2021-06-07 12:56:38 -0300

    ainda tentando fazer a requisiçãop de api

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class SearchComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http : HttpClient) { }
 
   ngOnInit(): void {
   }
